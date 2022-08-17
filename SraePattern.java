f27764909fcc16ab044f8b23fa2b03a127a4d5ef
Chetan Ambalkar <109157539+chetanambalkar@users.noreply.github.com>
2022-08-17 19:47:48 +0530

    Add files via upload

---
+++ SraePattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StarPattern {
+    public static void main() {
+        Scanner sc = new Scanner (System.in);
+        int n = nextInt ();
+        int m = nextInt ();
+        for (i=0; i<=n; i++) {
+            for (j=0; j<=m; j++)
+         {
+            System.out.println("*");
+         }
+        }
+    }
+}
---
+++ star.java
@@ -0,0 +1,14 @@
+import java. util .Scanner;
+
+class Star {
+    public static void main() {
+        for(int i=0; i<=4; i++)
+        {
+            for (int i=0; i<=5; i++)
+            {
+                System.out.println("*");
+            }
+        }
+        
+    }
+}
